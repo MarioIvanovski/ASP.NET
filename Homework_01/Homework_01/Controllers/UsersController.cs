edc0610f3c51d3ba8894c72e52ce8a26ca76bce7
MarioIvanovski <ivanovskimarioseptember2002@gmail.com>
2025-08-14 16:10:46 +0200

    Add files via upload

---
+++ Homework_01/Homework_01/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        
+        // GET api/users
+        [HttpGet]
+        public ActionResult<List<string>> GetAllUsers()
+        {
+            return Ok(StaticDb.Users);
+        }
+
+        // GET api/users/{index}
+        [HttpGet("{index}")]
+        public ActionResult<string> GetUserByIndex(int index)
+        {
+            if (index < 0 || index >= StaticDb.Users.Count)
+            {
+                return NotFound("User not found");
+            }
+            return Ok(StaticDb.Users[index]);
+        }
+    }
+}
+
---
+++ Homework_01/Homework_01/Data/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework_01
+{
+    public static class StaticDb
+    {
+        public static List<string> Users = new List<string>
+        {
+            "Mario",
+            "Bobsky",
+            "Dragan",
+            "Tose",
+            "Niki"
+        };
+    }
+}
